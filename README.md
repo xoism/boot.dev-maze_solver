962322fae979a4f5e412f491b5ccfe2ced439890
xoism <99417871+xoism@users.noreply.github.com>
2023-11-06 00:15:10 -0500

    ch 5 - cell class

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # boot.dev-maze_solver
-boot.dev guided project: build a mazee solver
+
+boot.dev guided project: build a maze solver
+
+# stuff to look into
+
+- [Cartesian product - Wikipedia](https://en.wikipedia.org/wiki/Cartesian_product)
+
+# references
+
+- [ChatGPT - generate all 4 length combos of True/False](https://chat.openai.com/share/df37ea1c-988d-4be3-b754-e6d8bc62e1ce)
+- [Generating all possibly length n combinations of two items in python - Stack Overflow](https://stackoverflow.com/questions/27757973/generating-all-possibly-length-n-combinations-of-two-items-in-python)
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
 import random
+
+from itertools import product
+
 from tkinter import Tk, BOTH, Canvas
+
+from colors import COLORS
 
 
 class Point:
@@ -8,14 +13,16 @@ class Point:
         self.x = x
         self.y = y
 
+    def __call__(self) -> (int, int):
+        return self.x, self.y
 
 class Line:
     def __init__(self, p1: Point, p2: Point):
         self.p1 = p1
         self.p2 = p2
 
-    def draw(self, canvas: Canvas, fill_color:str):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+    def draw(self, canvas: Canvas, fill_color:str = 'black'):
+        canvas.create_line(self.p1(), self.p2(), fill=fill_color, width=2)
         canvas.pack()
 
 
@@ -23,7 +30,7 @@ class Window:
     def __init__(self, width: int, height: int):
         self.__root = Tk()
         self.__root.title = 'boot.dev maze solver'
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.protocol('WM_DELETE_WINDOW', self.close)
         self.canvas = Canvas(self.__root, width=width, height=height)
         self.canvas.pack()
         self.window_is_running = False
@@ -40,8 +47,38 @@ class Window:
     def close(self):
         self.window_is_running = False
     
-    def draw_line(self, line: Line, fill_color:str):
+    def draw_line(self, line: Line, fill_color:str = 'black'):
         line.draw(self.canvas, fill_color)
+
+
+class Cell:
+    def __init__(self, top_left: Point, bottom_right: Point, window: Window):
+        self.left_wall = True
+        self.right_wall = True
+        self.top_wall = True
+        self.bottom_wall = True
+        self._top_left = top_left
+        self._bottom_right = bottom_right
+        self._top_right = Point(bottom_right.x, top_left.y)
+        self._bottom_left = Point(top_left.x, bottom_right.y)
+        self._win = window
+    
+    def draw(self, color:str = 'black'):
+        if self.left_wall:
+            self._win.draw_line(Line(self._top_left, self._bottom_left), fill_color=color)
+        if self.right_wall:
+            self._win.draw_line(Line(self._top_right, self._bottom_right), fill_color=color)
+        if self.top_wall:
+            self._win.draw_line(Line(self._top_left, self._top_right), fill_color=color)
+        if self.bottom_wall:
+            self._win.draw_line(Line(self._bottom_left, self._bottom_right), fill_color=color)
+        
+
+# https://chat.openai.com/share/df37ea1c-988d-4be3-b754-e6d8bc62e1ce
+# https://stackoverflow.com/questions/27757973/generating-all-possibly-length-n-combinations-of-two-items-in-python
+def generate_boolean_combinations():
+    for combination in product([True, False], repeat=4):
+        yield combination
 
 
 def main():
@@ -50,7 +87,14 @@ def main():
         p1 = Point(random.randrange(800), random.randrange(600))
         p2 = Point(random.randrange(800), random.randrange(600))
         line = Line(p1, p2)
-        win.draw_line(line, random.choice(['red', 'black', 'yellow', 'green', 'blue', 'black', 'orange', 'chartreuse', 'purple', 'brown']))
+        win.draw_line(line, random.choice(COLORS))
+    for combo in generate_boolean_combinations():
+        p1 = Point(random.randrange(800-30), random.randrange(600-30))
+        p2 = Point(p1.x + 30, p1.y + 30)
+        cell = Cell(p1, p2, win)
+        cell.left_wall, cell.right_wall, cell.top_wall, cell.bottom_wall = combo
+        cell.draw(random.choice(COLORS))
+
     win.wait_for_close()
 
 
